c91e3f49d8bfc63753acdcd0b3f991be5e8eef4c
unknown <sggp.kmiecik@gmail.com>
2021-02-18 21:31:23 +0100

    Added unit test for new model

---
+++ Tests/UnitTests/Users.spec.js
@@ -0,0 +1,54 @@
+const { expect } = require("chai");
+const chai = require("chai");
+chai.use(require("sinon-chai"));
+
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkUniqueIndex,
+  checkPropertyExists,
+} = require("sequelize-test-helpers");
+
+const UsersModel = require("../../Models/Users");
+
+describe("../Models/Genders", () => {
+  const Users = UsersModel(sequelize, dataTypes);
+  const users = new Users();
+
+  checkModelName(Users)("Users");
+
+  context("properties", () => {
+    ["id", "email", "password", "accountDeletedStatus"].forEach(
+      checkPropertyExists(users)
+    );
+  });
+
+  context("associations", () => {
+    const Genders = "user gender";
+
+    before(() => {
+      Users.associate({ Genders });
+    });
+
+    it("Defined a belongsTo association with Genders", () => {
+      expect(Users.belongsTo).to.have.been.calledWith(Genders);
+    });
+  });
+
+  context("associations", () => {
+    const TypesOfUserRoles = "type of user";
+
+    before(() => {
+      Users.associate({ TypesOfUserRoles });
+    });
+
+    it("Defined a belongsTo association with TypesOfUserRoles", () => {
+      expect(Users.belongsTo).to.have.been.calledWith(TypesOfUserRoles);
+    });
+  });
+
+  context("indexes", () => {
+    ["id_user", "user_email"].forEach(checkUniqueIndex(users));
+  });
+});
