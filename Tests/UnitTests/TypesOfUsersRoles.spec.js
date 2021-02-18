ac26b01cf778f113b49d5bcfad7b1d3913a6d72d
unknown <sggp.kmiecik@gmail.com>
2021-02-18 16:49:07 +0100

    Added unit test for users types model

---
+++ Tests/UnitTests/TypesOfUsersRoles.spec.js
@@ -0,0 +1,26 @@
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkUniqueIndex,
+  checkPropertyExists,
+} = require("sequelize-test-helpers");
+
+const TypesOfUsersRolesModel = require("../../Models/TypesOfUsersRoles");
+
+describe("../Models/TypesOfUsersRoles", () => {
+  const TypesOfUsersRoles = TypesOfUsersRolesModel(sequelize, dataTypes);
+  const typesOfUsersRoles = new TypesOfUsersRoles();
+
+  checkModelName(TypesOfUsersRoles)("TypesOfUsersRoles");
+
+  context("properties", () => {
+    ["id", "name"].forEach(checkPropertyExists(typesOfUsersRoles));
+  });
+
+  context("indexes", () => {
+    ["idTypeOfUserRole", "nameOfUserRole"].forEach(
+      checkUniqueIndex(typesOfUsersRoles)
+    );
+  });
+});
