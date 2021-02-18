f8b3ee1d05c5a478c510bf728c27cf2df6cfeb7d
unknown <sggp.kmiecik@gmail.com>
2021-02-18 14:56:12 +0100

    Added connection with database

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const sequelize = require("./Functions/Database/database");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log(app);
+    console.log("Database created!");
+  })
+  .catch((error) => {
+    throw new Error(error);
+  });
 
 const port = process.env.PORT || 3000;
 
