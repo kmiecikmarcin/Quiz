b4be1eba74ee8a9c58db6bbf1caa21a525a39f4f
unknown <sggp.kmiecik@gmail.com>
2021-02-18 21:31:07 +0100

    New model

---
+++ Models/Users.js
@@ -0,0 +1,55 @@
+const { Sequelize } = require("sequelize");
+const GendersModel = require("./Genders");
+const TypesOfUserRolesModel = require("./TypesOfUsersRoles");
+
+const model = (sequelize, DataTypes) => {
+  const Genders = GendersModel(sequelize, DataTypes);
+  const TypesOfUserRoles = TypesOfUserRolesModel(sequelize, DataTypes);
+  const Users = sequelize.define(
+    "Users",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+        field: "id_user",
+      },
+      email: {
+        type: DataTypes.STRING(100),
+        unique: true,
+        allowNull: false,
+        field: "user_email",
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "user_password",
+      },
+      accountDeletedStatus: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+        field: "account_deleted_status",
+      },
+    },
+    {
+      indexes: [
+        { unique: true, fields: ["id_user"] },
+        { unique: true, fields: ["user_email"] },
+      ],
+    }
+  );
+  Users.belongsTo(Genders);
+  Users.belongsTo(TypesOfUserRoles);
+
+  Users.associate = ({ Genders, TypesOfUserRoles }) => {
+    Users.belongsTo(Genders);
+    Users.belongsTo(TypesOfUserRoles);
+  };
+
+  return Users;
+};
+
+module.exports = model;
