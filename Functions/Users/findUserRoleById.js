98c003cc68b765ebc9141868d5e11a28f3744a1c
unknown <sggp.kmiecik@gmail.com>
2021-02-26 12:17:16 +0100

    Fix bug

--- Functions/Users/findUserRoleById.js
+++ Functions/Users/findUserRoleById.js
@@ -1,5 +1,5 @@
 async function findUserRoleById(TypesOfUsersRoles, userRoleId) {
-  const findUserRole = TypesOfUsersRoles.findOne({ id: userRoleId });
+  const findUserRole = TypesOfUsersRoles.findOne({ where: { id: userRoleId } });
   if (findUserRole !== null) {
     return findUserRole.name;
   }
