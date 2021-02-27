5d83c0c830565e46ea63321d01050a3fdaa9f12c
unknown <sggp.kmiecik@gmail.com>
2021-02-27 17:43:33 +0100

    Added unit tests for register

--- Tests/UnitTests/UnitTestsForMethods/register.spec.js
+++ Tests/UnitTests/UnitTestsForMethods/register.spec.js
@@ -23,4 +23,40 @@ describe("POST /register", () => {
         done();
       });
   });
+
+  it("Create user account with existing email in system", (done) => {
+    request(app)
+      .post("/quiz/users/register")
+      .send({
+        user_email: randomUserEmail,
+        user_password: "userPassword@",
+        confirm_password: "userPassword@",
+        user_gender: "Kobieta",
+        user_verification: true,
+      })
+      .then((res) => {
+        expect(res.statusCode).equal(400);
+        expect(res.body.Error).equal(
+          "Wprowadzony adres e-mail istnieje w systemie!"
+        );
+        done();
+      });
+  });
+
+  it("Create user account with incorrect gender data", (done) => {
+    request(app)
+      .post("/quiz/users/register")
+      .send({
+        user_email: randomUserEmail,
+        user_password: "userPassword@",
+        confirm_password: "userPassword@",
+        user_gender: "badGender",
+        user_verification: true,
+      })
+      .then((res) => {
+        expect(res.statusCode).equal(400);
+        expect(res.body.user_gender.msg).equal("Podano błędną wartość!");
+        done();
+      });
+  });
 });
