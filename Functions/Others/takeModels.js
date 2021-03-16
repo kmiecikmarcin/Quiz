ca8472ddd67b7f6e104f9e18ec0a56ce5a366585
unknown <sggp.kmiecik@gmail.com>
2021-03-16 10:37:16 +0100

    Added chapter model

--- Functions/Others/takeModels.js
+++ Functions/Others/takeModels.js
@@ -4,15 +4,18 @@ const TypesOfUsersRolesModel = require("../../Models/TypesOfUsersRoles");
 const GendersModel = require("../../Models/Genders");
 const UsersModel = require("../../Models/Users");
 const SchoolSubjectsModel = require("../../Models/Users");
+const ChaptersModel = require("../../Models/Chapters");
 
 const TypesOfUsersRoles = TypesOfUsersRolesModel(sequelize, DataTypes);
 const Genders = GendersModel(sequelize, DataTypes);
 const Users = UsersModel(sequelize, DataTypes);
 const SchoolSubjects = SchoolSubjectsModel(sequelize, DataTypes);
+const Chapters = ChaptersModel(sequelize, DataTypes);
 
 module.exports = {
   TypesOfUsersRoles,
   Genders,
   Users,
   SchoolSubjects,
+  Chapters,
 };
