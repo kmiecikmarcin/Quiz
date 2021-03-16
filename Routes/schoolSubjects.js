77a5982171963e3a20d4b32812eb3e5844066982
unknown <sggp.kmiecik@gmail.com>
2021-03-16 16:32:41 +0100

    Added new GET

--- Routes/schoolSubjects.js
+++ Routes/schoolSubjects.js
@@ -1,5 +1,39 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const verifyToken = require("../Functions/Others/verifyToken");
+const checkExistsOfUserEmail = require("../Functions/Users/checkExistsOfUserEmail");
+const Model = require("../Functions/Others/takeModels");
+const takeAllSchoolSubjects = require("../Functions/SchoolSubjects/takeAllSchoolSubjects");
 
 const router = express.Router();
+
+router.get("/subjects", verifyToken, (req, res) => {
+  jwt.verify(
+    req.token,
+    process.env.S3_SECRETKEY,
+    async (jwtError, authData) => {
+      if (jwtError) {
+        res.status(403).json({ Error: "Błąd uwierzytelniania!" });
+      } else {
+        const checkUser = await checkExistsOfUserEmail(
+          Model.Users,
+          authData.email
+        );
+        if (checkUser !== false) {
+          const takeSchoolSubjects = await takeAllSchoolSubjects(
+            Model.SchoolSubjects
+          );
+          if (takeSchoolSubjects !== false) {
+            res.status(200).json(takeSchoolSubjects);
+          } else {
+            res.status(501).json({ Error: "Brak przedmiotów szkolnych!" });
+          }
+        } else {
+          res.status(400).json({ Error: "Użytkownik nie istnieje!" });
+        }
+      }
+    }
+  );
+});
 
 module.exports = router;
