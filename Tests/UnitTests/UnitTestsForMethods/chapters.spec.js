e8cabe0a31ba28190105473fae7d336d1475e145
unknown <sggp.kmiecik@gmail.com>
2021-03-17 14:20:20 +0100

    Added unit tests

---
+++ Tests/UnitTests/UnitTestsForMethods/chapters.spec.js
@@ -0,0 +1,31 @@
+const { expect } = require("chai");
+const request = require("supertest");
+const app = require("../../../app");
+const userToken = require("./login.spec");
+
+describe("GET /chapters", () => {
+  it("Take all chapters", (done) => {
+    request(app)
+      .get("/quiz/schoolSubcjects/chapters")
+      .set("Content-Type", "application/json")
+      .set("Authorization", `Bearer ${userToken.token}`)
+      .then((res) => {
+        expect(res.statusCode).equal(200);
+        expect(res.body.Token).to.not.equal(null);
+        done();
+      });
+  });
+
+  it("Try to take chapters without authorization", (done) => {
+    request(app)
+      .get("/quiz/schoolSubcjects/chapters")
+      .set("Content-Type", "application/json")
+      .set("Authorization", "")
+      .then((res) => {
+        expect(res.statusCode).equal(403);
+        expect(res.body).to.have.property("Error");
+        expect(res.body.Error).equal("Błąd uwierzytelniania!");
+        done();
+      });
+  });
+});
