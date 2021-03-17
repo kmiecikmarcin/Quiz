0e6ba745ef448661ce311218ff37132f991af08d
unknown <sggp.kmiecik@gmail.com>
2021-03-17 12:01:35 +0100

    Added GET /chapters

--- Routes/schoolSubjects.js
+++ Routes/schoolSubjects.js
@@ -4,6 +4,7 @@ const verifyToken = require("../Functions/Others/verifyToken");
 const checkExistsOfUserEmail = require("../Functions/Users/checkExistsOfUserEmail");
 const Model = require("../Functions/Others/takeModels");
 const takeAllSchoolSubjects = require("../Functions/SchoolSubjects/takeAllSchoolSubjects");
+const takeAllChapters = require("../Functions/SchoolSubjects/takeAllChapters");
 
 const router = express.Router();
 
@@ -44,6 +45,33 @@ router.get("/subjects", verifyToken, (req, res) => {
             res.status(200).json(takeSchoolSubjects);
           } else {
             res.status(501).json({ Error: "Brak przedmiotów szkolnych!" });
+          }
+        } else {
+          res.status(400).json({ Error: "Użytkownik nie istnieje!" });
+        }
+      }
+    }
+  );
+});
+
+router.get("/chapters", verifyToken, (req, res) => {
+  jwt.verify(
+    req.token,
+    process.env.S3_SECRETKEY,
+    async (jwtError, authData) => {
+      if (jwtError) {
+        res.status(403).json({ Error: "Błąd uwierzytelniania!" });
+      } else {
+        const checkUser = await checkExistsOfUserEmail(
+          Model.Users,
+          authData.email
+        );
+        if (checkUser !== false) {
+          const takeChapters = await takeAllChapters(Model.Chapters);
+          if (takeChapters !== false) {
+            res.status(200).json(takeChapters);
+          } else {
+            res.status(404).json({ Error: "Brak rozdziałów w systemie!" });
           }
         } else {
           res.status(400).json({ Error: "Użytkownik nie istnieje!" });
