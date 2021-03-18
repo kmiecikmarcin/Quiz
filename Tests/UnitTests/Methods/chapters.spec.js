b123b91cca7ab4a90fe3afcd3f8dee9ca0a0756b
unknown <sggp.kmiecik@gmail.com>
2021-03-18 14:41:26 +0100

    Added unit test with teacher permissions

--- Tests/UnitTests/Methods/chapters.spec.js
+++ Tests/UnitTests/Methods/chapters.spec.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 const request = require("supertest");
 const app = require("../../../app");
 const userToken = require("./login.spec");
+const teacherToken = require("./loginAsTeacher.spec");
 
 describe("GET /chapters", () => {
   it("Take all chapters", (done) => {
@@ -9,6 +10,18 @@ describe("GET /chapters", () => {
       .get("/quiz/schoolSubjects/chapters")
       .set("Content-Type", "application/json")
       .set("Authorization", `Bearer ${userToken.token}`)
+      .then((res) => {
+        expect(res.statusCode).equal(200);
+        expect(res.body.Token).to.not.equal(null);
+        done();
+      });
+  });
+
+  it("Take all chapter with teacher permissions teacher", (done) => {
+    request(app)
+      .get("/quiz/schoolSubjects/chapters")
+      .set("Content-Type", "application/json")
+      .set("Authorization", `Bearer ${teacherToken.teacherToken}`)
       .then((res) => {
         expect(res.statusCode).equal(200);
         expect(res.body.Token).to.not.equal(null);
