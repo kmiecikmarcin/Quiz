eeb0c6fe78a06ea11c934a489c3602d93a6304f5
unknown <sggp.kmiecik@gmail.com>
2021-03-18 14:42:42 +0100

    Added unit test with teacher permissions

--- Tests/UnitTests/Methods/topics.spec.js
+++ Tests/UnitTests/Methods/topics.spec.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 const request = require("supertest");
 const app = require("../../../app");
 const userToken = require("./password.spec");
+const teacherToken = require("./loginAsTeacher.spec");
 
 describe("GET /topics", () => {
   it("Take all topics", (done) => {
@@ -9,6 +10,18 @@ describe("GET /topics", () => {
       .get("/quiz/schoolSubjects/topics")
       .set("Content-Type", "application/json")
       .set("Authorization", `Bearer ${userToken.token}`)
+      .then((res) => {
+        expect(res.statusCode).equal(200);
+        expect(res.body.Token).to.not.equal(null);
+        done();
+      });
+  });
+
+  it("Take all topics with teacher permissions", (done) => {
+    request(app)
+      .get("/quiz/schoolSubjects/topics")
+      .set("Content-Type", "application/json")
+      .set("Authorization", `Bearer ${teacherToken.teacherToken}`)
       .then((res) => {
         expect(res.statusCode).equal(200);
         expect(res.body.Token).to.not.equal(null);
