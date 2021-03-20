01f7c18b59ff7c32810741feee0f62b5e985da13
unknown <sggp.kmiecik@gmail.com>
2021-03-20 17:18:56 +0100

    Fix

--- Functions/SchoolSubjects/chapterToBeDeleted.js
+++ Functions/SchoolSubjects/chapterToBeDeleted.js
@@ -1,5 +1,8 @@
 async function chapterToBeDeleted(Chapters, chapterId) {
-  const deleteChapter = await Chapters.destroy({ where: { id: chapterId } });
+  const deleteChapter = await Chapters.update(
+    { toRemove: true },
+    { where: { id: chapterId } }
+  );
   if (deleteChapter !== null) {
     return true;
   }
